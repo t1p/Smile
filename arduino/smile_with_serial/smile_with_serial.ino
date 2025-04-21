7f19a7f5b2281987cc0d59e924b72febdf7799bd
TIP <trapeznikovip@gmail.com>
2025-04-22 00:30:38 +0200

    Добавлена схема, внесены правки для подавления дребезга кнопок

--- arduino/smile_with_serial/smile_with_serial.ino
+++ arduino/smile_with_serial/smile_with_serial.ino
@@ -10,85 +10,96 @@ int good = 0;
 
 void setup()
 {
-  Serial.begin(9600);
-  
-  pinMode(2, INPUT);
-  pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(7, INPUT);
-  pinMode(4, OUTPUT);
-  pinMode(12, INPUT);
-  pinMode(8, OUTPUT);
+  Serial.begin(9600); // Инициализация монитора порта
+
+  pinMode(2, INPUT);    // кнопка "bad" (S_BAD_B)
+  pinMode(LED_BUILTIN, OUTPUT); // LED встроенный (D1 RED)
+  pinMode(7, INPUT);    // кнопка "soso" (S_SOSO_B)
+  pinMode(4, OUTPUT);    // LED для "soso" (D2 WHITE)
+  pinMode(12, INPUT);   // кнопка "good" (S_GOOD_B)
+  pinMode(8, OUTPUT);    // LED для "good" (D3 BLUE)
 
   bad = 0;
   soso = 0;
   good = 0;
-  lcd_1.init();            // инициализация
-  lcd_1.backlight();       // включить подсветку
-  lcd_1.setCursor(1, 0);
-  lcd_1.print("Self-awareness");
+  lcd_1.init();
+  lcd_1.backlight();
   
-  // Вывод информации о файле
+  // Вывод информации о файле и дате на LCD и в Serial
+  lcd_1.setCursor(0, 0);
+  lcd_1.print("Self-awareness");
+  //lcd_1.print("File:");
+  //lcd_1.setCursor(0, 1);
+  //lcd_1.print(__FILE__);
+  
   Serial.print("File: ");
   Serial.println(__FILE__);
   Serial.print("Date: ");
   Serial.println(__DATE__);
   
-  lcd_1.setCursor(0, 1);
-  lcd_1.print(__FILE__);
-  lcd_1.setCursor(0, 0);
-  
-  delay(1000); // Wait for 1000 millisecond(s)
+  delay(2000); // Ждем, чтобы прочитать информацию
   lcd_1.clear();
+
+  lcd_1.setCursor(1, 0);
+  lcd_1.print("bad");
+  lcd_1.setCursor(5, 0);
+  lcd_1.print("soso");
+  lcd_1.setCursor(10, 0);
+  lcd_1.print("good");
 }
 
 void loop()
 {
-  // read the state of the pushbutton value
+  // Обработка кнопки BAD (S_BAD_B)
   buttonState = digitalRead(2);
-  if (buttonState == HIGH) {
-    bad = (bad + 1);
-    lcd_1.setCursor(0, 0);
-    lcd_1.print("bad");
+  if (buttonState == HIGH) { // Кнопка нажата (при INPUT и стягивающем резисторе)
+    bad++;
+    lcd_1.setCursor(1, 1);
+    lcd_1.print("   "); // Очищаем предыдущее значение
     lcd_1.setCursor(1, 1);
     lcd_1.print(bad);
     digitalWrite(LED_BUILTIN, HIGH);
     
     Serial.print("bad: ");
     Serial.println(bad);
+    delay(300); // Задержка для подавления дребезга
   } else {
     digitalWrite(LED_BUILTIN, LOW);
   }
 
+  // Обработка кнопки SOSO (S_SOSO_B)
   buttonState = digitalRead(7);
-  if (buttonState == HIGH) {
-    soso = (soso + 1);
-    digitalWrite(4, HIGH);
-    lcd_1.setCursor(5, 0);
-    lcd_1.print("soso");
-    lcd_1.setCursor(6, 1);
+  if (buttonState == HIGH) { // Кнопка нажата
+    soso++;
+    lcd_1.setCursor(5, 1);
+    lcd_1.print("   "); // Очищаем предыдущее значение
+    lcd_1.setCursor(5, 1);
     lcd_1.print(soso);
-    digitalWrite(LED_BUILTIN, HIGH);
+    digitalWrite(4, HIGH);
     
     Serial.print("soso: ");
     Serial.println(soso);
+    delay(300); // Задержка для подавления дребезга
   } else {
     digitalWrite(4, LOW);
   }
 
+  // Обработка кнопки GOOD (S_GOOD_B)
   buttonState = digitalRead(12);
-  if (buttonState == HIGH) {
-    good = (good + 1);
-    digitalWrite(8, HIGH);
-    lcd_1.setCursor(10, 0);
-    lcd_1.print("good");
-    lcd_1.setCursor(11, 1);
+  if (buttonState == HIGH) { // Кнопка нажата
+    good++;
+    lcd_1.setCursor(10, 1);
+    lcd_1.print("   "); // Очищаем предыдущее значение
+    lcd_1.setCursor(10, 1);
     lcd_1.print(good);
-    digitalWrite(LED_BUILTIN, HIGH);
+    digitalWrite(8, HIGH);
     
     Serial.print("good: ");
     Serial.println(good);
+    delay(300); // Задержка для подавления дребезга
   } else {
     digitalWrite(8, LOW);
   }
-  delay(500);
+  
+  delay(10); // Небольшая задержка в конце цикла
 }
